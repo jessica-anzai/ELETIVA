2de1e867743fee9d5059d3482c45438565fa2e6a
Jessica Anzai <jesteruan@gmail.com>
2025-09-21 10:15:35 -0300

    ok

---
+++ ListaPHP-2/atividade1.php
@@ -0,0 +1,99 @@
+<?php 
+    include("cabecalho.php");
+?>
+  <form method="post">
+    <div class="p-3 container rounded bg-success p-2 text-dark bg-opacity-10 border mt-5 w-75 mb-5">
+        <div class="row mt-3">
+            <div class="col-3">
+                <label for="n1" class="form-label">Digite o primeiro número</label>
+                <input type="number" name="n1" id="n1" class="form-control">
+            </div>
+
+            <div class="col-3">
+                <label for="n2" class="form-label">Digite o segundo número</label>
+                <input type="number" name="n2" id="n2" class="form-control">
+            </div>
+
+            <div class="col-3">
+                <label for="n3" class="form-label">Digite o terceiro número</label>
+                <input type="number" name="n3" id="n3" class="form-control">
+            </div>
+
+            <div class="col-3">
+                <label for="n4" class="form-label">Digite o quarto número</label>
+                <input type="number" name="n4" id="n4" class="form-control">
+            </div>
+        </div>
+        
+        <div class="row mt-3">
+            <div class="col-3">
+                <label for="n5" class="form-label">Digite o quinto número</label>
+                <input type="number" name="n5" id="n5" class="form-control">
+            </div>
+
+            <div class="col-3">
+                <label for="n6" class="form-label">Digite o sexto número</label>
+                <input type="number" name="n6" id="n6" class="form-control">
+            </div>
+
+            <div class="col-3">
+                <label for="n7" class="form-label">Digite o sétimo número</label>
+                <input type="number" name="n7" id="n7" class="form-control">
+            </div>
+        </div>
+
+            <div class="row mt-3 mb-3">
+                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                    <button class="btn btn-success me-md-2" type="submit">Enviar</button>
+                    <button class="btn btn-outline-secondary" type="clear">Cancelar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>  
+
+<?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $n1 = $_POST['n1'];
+    $n2 = $_POST['n2'];
+    $n3 = $_POST['n3'];
+    $n4 = $_POST['n4'];
+    $n5 = $_POST['n5'];
+    $n6 = $_POST['n6'];
+    $n7 = $_POST['n7'];
+    
+    $menor = $n1;
+    $posicao = 1;
+    
+    if($n2 < $menor){
+        $menor = $n2;
+        $posicao = 2;
+    }
+    if($n3 < $menor){
+        $menor = $n3;
+        $posicao = 3;
+    }
+    if($n4 < $menor){
+        $menor = $n4;
+        $posicao = 4;
+    }
+    if($n5 < $menor){
+        $menor = $n5;
+        $posicao = 5;
+    }
+    if($n6 < $menor){
+        $menor = $n6;
+        $posicao = 6;
+    }
+    if($n7 < $menor){
+        $menor = $n7;
+        $posicao = 7;
+    }
+    
+    echo '<div class="p-3 container rounded bg-success p-2 text-dark bg-opacity-10 border mt-5 w-75 mb-5 text-center">';
+    echo "<label class='form-label fs-3'>Menor número: $menor - Posição: $posicao</label>";
+    echo '</div>';
+    
+  }
+    include("rodape.php");
+?>
---
+++ ListaPHP-2/cabecalho.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Lista 2 - PHP</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-success">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Lista 2 - PHP</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Alternar navegação">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="dropdown1" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Atividades IF
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="dropdown1">
+              <li><a class="dropdown-item" href="atividade1.php">Atividade 1</a></li>
+              <li><a class="dropdown-item" href="atividade2.php">Atividade 2</a></li>
+              <li><a class="dropdown-item" href="atividade3.php">Atividade 3</a></li>
+              <li><a class="dropdown-item" href="atividade4.php">Atividade 4</a></li>
+            </ul>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="dropdown2" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Atividade Switch
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="dropdown2">
+              <li><a class="dropdown-item" href="atividade5.php">Atividade 5</a></li>
+            </ul>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="dropdown3" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Atividades Repetição
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="dropdown3">
+              <li><a class="dropdown-item" href="atividade6.php">Atividade 6</a></li>
+              <li><a class="dropdown-item" href="atividade7.php">Atividade 7</a></li>
+              <li><a class="dropdown-item" href="atividade8.php">Atividade 8</a></li>
+              <li><a class="dropdown-item" href="atividade9.php">Atividade 9</a></li>
+              <li><a class="dropdown-item" href="atividade10.php">Atividade 10</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="container-fluid py-3">
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</body>
+
+</html>
