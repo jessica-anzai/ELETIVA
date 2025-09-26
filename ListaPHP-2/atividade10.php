372af5ead914f8a3c6cc67018e99b321feb86919
jessica-anzai <jesteruan@gmail.com>
2025-09-25 21:06:05 -0300

    25-09-2025

--- ListaPHP-2/atividade10.php
+++ ListaPHP-2/atividade10.php
@@ -1,7 +1,36 @@
 <?php 
     include("cabecalho.php");
 ?>
-
+ <form method="post">
+    <div class="p-3 container rounded bg-success p-2 text-dark bg-opacity-10 border mt-5 w-75 mb-5">
+        <h2 class="text-center">Atividade 10</h2>
+        <div class="row justify-content-center">
+            <div class="col-5 4mb-3">
+                <label for="num" class="form-label">Informe um número</label>
+                <input type="number" id="num" name="num" class="form-control">
+            </div>
+            <div class="row mt-3 mb-3">
+                <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+                    <button class="btn btn-success me-md-2" type="submit">Enviar</button>
+                    <button class="btn btn-outline-secondary" onclick="location.reload(true);">Atualizar página</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
 <?php
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+        $num = $_POST['num'];
+
+        echo '<div class="p-3 container rounded bg-success p-2 text-dark bg-opacity-10 border mt-5 w-75 mb-5 text-center">';
+        echo "<h3>RESPOSTA</h3>";
+        for ($i = 1; $i <= 10; $i++) {
+            $result = $num*$i;
+            echo "<p>$num x $i = $result</p>";
+        }
+        echo "</div>";
+    }
     include("rodape.php");
 ?>
